660002bc46b41a19f26205c56adc0cba30caaf7a
ZAHID DE JESUS AVALOS HERNANDEZ <l19350283@tuxtepec.tecnm.mx>
2022-10-18 10:55:46 -0500

    consultas 1 y por ID

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,47 @@
 const { request, response } = require("express");
+const pool=require("../db/connection")
+const getUser = async(req=request,res=response)=>{
+    
+    let conn;
 
-const getUser = (req=request,res=response)=>{
-    console.log("Función getUser")
-    res.json({msg:"Función getUser"})
+    try{
+        conn = await pool.getConnection()
+        const users = await conn.query("SELECT * FROM usuarios",(error)=>{throw new error})
+        if(!users){
+            res.status(404).json({msg:"No se encontraron registros"})
+            return
+        }
+        res.json({users})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
 }
 
-module.exports={getUser}
+
+const getUserByID = async (req=request,res=response)=>{
+    const {id}=req.params
+    let conn;
+
+    try{
+        conn = await pool.getConnection()
+        const [user] = await conn.query(`SELECT * FROM usuarios WHERE ID=${id}`,(error)=>{throw new error})
+        if(!user){
+            res.status(404).json({msg:`No se encontró registro con el ID ID=${id}`})
+            return
+        }
+        res.json({user})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
+}
+module.exports={getUser,getUserByID}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,10 @@
 const {Router} = require("express")
-const {getUser} = require("../controllers/usuarios")
+const {getUser, getUserByID} = require("../controllers/usuarios")
 const router = Router()
 
 //http://localhost:4000/api/v1/usuarios
 
 router.get("/", getUser)
+router.get("/id/:id", getUserByID)
 
 module.exports = router
